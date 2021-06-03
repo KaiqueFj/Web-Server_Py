d120d8e6f15955e532016fe7b8d9977c78473594
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-03 13:20:05 -0300

    updating pages

--- server.py
+++ server.py
@@ -9,19 +9,10 @@ def my_home():
     return render_template('index.html')
 
 
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
 
-
-@app.route("/works.html")
-def work():
-    return render_template('works.html')
-
-
-@app.route("/contact.html")
-def contact():
-    return render_template('contact.html')
 
 # @app.route("/favicon.ico")
 # def blog():
