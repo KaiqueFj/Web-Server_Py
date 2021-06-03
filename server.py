68a26a7855855ff0217c29d8f525656a6ae161c4
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-03 14:57:56 -0300

    form already working

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 print(__name__)
@@ -12,6 +12,17 @@ def my_home():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('thank_you.html')
+    else:
+        return 'something went wrong, try again'
+
+
 
 
 # @app.route("/favicon.ico")
