f5c2efbbcb36f48c3df129fabd88c004c042e447
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-03 10:44:58 -0300

    updating

--- server.py
+++ server.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Michal Jordan is the Goat!</p>"
+    return render_template('index.html')
+
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/favicon.ico")
+def blog():
+    return render_template('these are my thoughts')
+
+
+
+
