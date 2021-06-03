90486b8e7ee085fd66fa98b5dffe7d638bdc1bd2
KaiqueFj <kaiquelferraz@Hotmail.com>
2021-06-03 11:14:39 -0300

    added favicon and css

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 print(__name__)
@@ -6,16 +6,17 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
+    print(url_for('static', filename='favicon.ico'))
     return render_template('index.html')
-
+    
 
 @app.route("/about.html")
 def about():
     return render_template('about.html')
 
-@app.route("/favicon.ico")
-def blog():
-    return render_template('these are my thoughts')
+# @app.route("/favicon.ico")
+# def blog():
+#     return render_template('these are my thoughts')
 
 
 
